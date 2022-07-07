107074ae8c2f5436d5d558c6c98f1f9c7c4394d5
Your Name <ishwarp@smbxl.com>
2022-07-07 15:12:15 +0530

    C-0

---
+++ js/script.js
@@ -0,0 +1,153 @@
+$(function () {
+  // jQuery goes here...
+
+  // Uncomment this line to fade out the red box on page load
+
+  // $(`.red-box`).fadeTo(1000, 0.25);
+  // $(`.green-box`).delay(1000).fadeTo(1000, 0.5);
+  // $(`.red-box`)
+  //   .delay(2000)
+  //   .fadeTo(1000, 0.25, function () {
+  //     $(`.green-box`).fadeTo(1000, 0.5, function () {
+  //       $(`.blue-box`).fadeTo(1000, 0.75, function () {
+  //         // alert(`Ìt's the final callbaack`);
+  //       });
+  //     });
+  //   });
+  // $(`.lightbox`).delay(500).fadeIn(1000);
+  //---------------box swap-------------------//
+  setInterval(function () {
+    $(`.blue-box`).animate(
+      {
+        "margin-left": "-=330px",
+        opacity: "1",
+      },
+      1000,
+      "linear"
+    );
+    $(`.red-box`).delay(1000).animate(
+      {
+        "margin-left": "+=420px",
+        opacity: "1",
+      },
+      1000
+    );
+    $(`.green-box`).delay(2000).animate(
+      {
+        "margin-left": "-=220px",
+        opacity: "1",
+      },
+      1000
+    );
+    ////
+    $(`.blue-box`).delay(3000).animate(
+      {
+        "margin-left": "+=330px",
+      },
+      1000,
+      "linear"
+    );
+    $(`.red-box`).delay(4000).animate(
+      {
+        "margin-left": "-=420px",
+      },
+      1000
+    );
+    $(`.green-box`).delay(5000).animate(
+      {
+        "margin-left": "+=220px",
+      },
+      1000
+    );
+  }, 15000);
+
+  //---------------CSS in JS------------------//
+  $(`.red-box,.green-box,.blue-box`).css(`border-radius`, `55px`);
+  $(`.red-box,.green-box,.blue-box`).css(`text-align`, `center`);
+  $(`.red-box,.green-box,.blue-box`).css(`position`, `relative`);
+  $(`.text`).css("position", "absolute");
+  $(`.text`).css("top", "50%");
+  $(`.text`).css("left", "50%");
+  $(`.text`).css("transform", "translate(-50%, -50%)");
+
+  $("#list").parent().css("background-color", "#fff111");
+  $(`li`)
+    .first()
+    .filter(function (index) {
+      return true;
+    })
+    .css(`color`, `#00d800`);
+  $(`li`)
+    .eq(4 + 5)
+    .filter(function (index) {
+      return true;
+    })
+    .css(`color`, `#d12211`);
+
+  $(`p`).css("background-color", "#228b22");
+  $(`p`).css(`color`, `#f1f1f1`);
+  $(`p`).css(`border-radius`, `25px`);
+  $(`p`).css(`padding`, `25px`);
+  $(`input[type=submit]`).css(`background-color`, `#009f6b`);
+  $(`input, textarea`).css(`border-radius`, `8px`);
+  $(`input[type=submit]`).css(`border-radius`, `25px`);
+  $(`input[type=submit]`).css(`color`, `#f1f1f1`);
+
+  //--------------------DOM---------------------//
+
+  $(`ul ul:first`).append("<li>jQuery appended values</li>");
+  $("<li>jQuery prepend values</li>").prependTo($(`ul ul:last`));
+  $("<h3>Ishwar Patro</h3>").prependTo($(`#content`)).css(`color`, `#400404`);
+  $(`.red-box`).after(
+    `<div class="red-box"><div class="text">after</div></div>`
+  );
+  $(`.blue-box`).before(function () {
+    return `<div class="blue-box"><div class="text">before</div></div>`;
+  });
+  $(`#content p:first`).after($(`#list`));
+  $(`#list2 ul li:first`).replaceWith(`<li>REPLACED ITEAM</li>`);
+  // $(`#list2:last`).replaceWith(`<li>REPLACED ITEAM</li>`);
+  $(`#list2 ul:last`).remove();
+  $(`.green-box,.blue-box,.red-box`).empty();
+  // $(`.green-box`).prev().prev().remove();
+  // $(`.green-box`).next().next().remove();
+  $(`.green-box`).prev().remove();
+  $(`.green-box`).next().remove();
+
+  let l = $(`p`);
+  console.log(l.attr(`p`));
+
+  let checkb = $(`input[type=checkbox]`);
+  console.log(checkb.prop(`checked`));
+
+  let v = $(`input[type=range]`);
+  console.log(v.val());
+
+  let galleryImage = $(`.gallery`).find(`img`).first();
+  let images = [
+    "images/laptop-mobile_small.jpg",
+    "images/laptop-on-table_small.jpg",
+    "images/people-office-group-team_small.jpg",
+    "images/login-page-bg.jpg",
+  ];
+
+  let count = 0;
+  setInterval(function () {
+    count = (count + 1) % images.length;
+    galleryImage.fadeOut(function () {
+      $(this).attr(`src`, images[count]);
+      $(this).fadeIn();
+    });
+    console.log(galleryImage.attr(`src`));
+  }, 2500);
+  //------------------DOM2--------------------//
+  let firstP = $(`p:first`);
+  console.log(firstP.text());
+  console.log(firstP.html());
+
+  firstP.html(`<h1><strong>Hello World!!</strong><h1>`);
+  //------------------EVENT HANDELING----------//
+  $(`#btn-click`).click(function () {
+    alert(`A button was clicked`);
+  });
+});
