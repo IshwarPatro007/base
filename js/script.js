2469d55d27616fd09b51cd96b595b691e5ef37ee
Your Name <ishwarp@smbxl.com>
2022-07-08 14:22:26 +0530

    C-5

--- js/script.js
+++ js/script.js
@@ -271,18 +271,32 @@ $(function () {
       alert(`Ajax call failed`);
     });
   //---------------------POKE API-----------------------//
-  var pokeapiURL = `https://pokeapi.co/api/v2/generation/3`;
+  let pokeapiURL = `https://pokeapi.co/api/v2/generation/3`;
+  let pokemonByName = `https://pokeapi.co/api/v2/pokemon/`;
 
   $.getJSON(pokeapiURL).done(function (data) {
     // console.log(data);
     $.each(data.pokemon_species, function (index, pokemon) {
       let name = pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1);
-      if (name == "Jirachi") {
-        name.toUpperCase();
-      }
-      let li = $(`<li>`).html(
-        "Pokemon species no. " + (index + 1) + " is " + name
-      );
+      let link = $(`<a>`)
+        .attr(`id`, pokemon.name)
+        .attr(`href`, `#`)
+        .append($(`<strong>`).text(name));
+      let li = $(`<li>`)
+        .html("Pokemon species no. " + (index + 1) + " is ")
+        .append(link);
+      link.click(function (event) {
+        $.getJSON(pokemonByName + pokemon.name).done(function (details) {
+          console.log(name, details);
+          let pokemonDiv = $(`#pokemon-details`);
+          pokemonDiv.empty();
+          pokemonDiv.append(`<h2>` + name + `</h2>`);
+          pokemonDiv.append(
+            `<img src='` + details.sprites.front_default + `'>`
+          );
+        });
+        event.preventDefault();
+      });
       li.appendTo(`#pokemon`);
     });
   });
